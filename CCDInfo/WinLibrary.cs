34ab4b94c5bfbf4acb429b9363495cb23df3c382
Terry MacDonald <terry.macdonald@cosive.com>
2021-07-10 17:21:25 +1200

    Removed Desharp

--- CCDInfo/WinLibrary.cs
+++ CCDInfo/WinLibrary.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using Desharp;
 using Microsoft.Win32.SafeHandles;
 using DisplayMagicianShared;
 using System.ComponentModel;
