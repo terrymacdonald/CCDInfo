e89101bf9428af3e537911e8606ac4823a2914b0
Terry MacDonald <terry.macdonald@cosive.com>
2021-09-10 21:09:58 +1200

    Update version

--- CCDInfo/Program.cs
+++ CCDInfo/Program.cs
@@ -45,10 +45,10 @@ namespace CCDInfo
             NLog.LogManager.Configuration = config;
             
             // Start the Log file
-            SharedLogger.logger.Info($"CCDInfo/Main: Starting CCDInfo v1.0.6");
+            SharedLogger.logger.Info($"CCDInfo/Main: Starting CCDInfo v1.0.7");
 
             
-            Console.WriteLine($"\nCCDInfo v1.0.6");
+            Console.WriteLine($"\nCCDInfo v1.0.7");
             Console.WriteLine($"==============");
             Console.WriteLine($"By Terry MacDonald 2021\n");
 
