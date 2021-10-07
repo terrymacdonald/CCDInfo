644a37ff122e03ee82cd18f7aed714c347725667
Terry MacDonald <terry.macdonald@cosive.com>
2021-10-07 20:43:05 +1300

    Updated the CCD and WinLibrary with latest updates
    These updates are simple spelling updates to keep this file in step with DisplayMagician changes.

--- CCDInfo/Program.cs
+++ CCDInfo/Program.cs
@@ -45,10 +45,10 @@ namespace CCDInfo
             NLog.LogManager.Configuration = config;
             
             // Start the Log file
-            SharedLogger.logger.Info($"CCDInfo/Main: Starting CCDInfo v1.0.7");
+            SharedLogger.logger.Info($"CCDInfo/Main: Starting CCDInfo v1.0.8");
 
             
-            Console.WriteLine($"\nCCDInfo v1.0.7");
+            Console.WriteLine($"\nCCDInfo v1.0.8");
             Console.WriteLine($"==============");
             Console.WriteLine($"By Terry MacDonald 2021\n");
 
