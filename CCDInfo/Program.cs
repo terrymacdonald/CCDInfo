21a5d10c4879a30a8b831aa24c4a4e54de7eca90
Terry MacDonald <terry.macdonald@cosive.com>
2022-06-02 19:58:17 +1200

    Made TaskBarLayout more robust

--- CCDInfo/Program.cs
+++ CCDInfo/Program.cs
@@ -45,10 +45,10 @@ namespace CCDInfo
             NLog.LogManager.Configuration = config;
             
             // Start the Log file
-            SharedLogger.logger.Info($"CCDInfo/Main: Starting CCDInfo v1.7.6");
+            SharedLogger.logger.Info($"CCDInfo/Main: Starting CCDInfo v1.7.7");
 
             
-            Console.WriteLine($"\nCCDInfo v1.7.6");
+            Console.WriteLine($"\nCCDInfo v1.7.7");
             Console.WriteLine($"==============");
             Console.WriteLine($"By Terry MacDonald 2022\n");
 
